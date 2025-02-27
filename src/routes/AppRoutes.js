f96511fd10d44f21715dd89fa540eac73a472a38
Hetzon Daniel Valenzuela Barrios <hetzondaniel46@gmail.com>
2025-02-27 06:59:51 -0600

    se arreglo probelmas con login

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,14 +1,17 @@
 // src/routes/AppRoutes.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../modules/Auth/pages/Login";        // Importa tu Login
 import Dashboard from "../modules/Dashboard/pages/Dashboard";
-// Importa también Login u otras vistas
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Inicio / Login</div>} />
+        {/* Muestra el Login en la ruta raíz */}
+        <Route path="/" element={<Login />} />
+
+        {/* Dashboard en /dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
